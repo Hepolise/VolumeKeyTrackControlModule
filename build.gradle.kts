b9e00568ce09ee2e8f333f293f12f4fdec9ae5f3
Nikita <Hepolise@users.noreply.github.com>
2025-01-05 11:43:38 +0000

    Refactor build.gradle to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("com.android.application") version "8.7.3" apply false
+    id("com.android.library") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+}
+
+val versionName = "1.15.2"
+val versionCode = 9
+
+rootProject.ext.set("appVersionName", versionName)
+rootProject.ext.set("appVersionCode", versionCode)
+
+tasks.register("getVersion") {
+    doLast {
+        val versionFile = file("app/build/version.txt")
+        versionFile.parentFile.mkdirs()
+        if (!versionFile.exists()) {
+            versionFile.createNewFile()
+        }
+        versionFile.writeText(versionName)
+    }
+}
